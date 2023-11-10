47c13077fa6ed3932c31de6dd0e26afbc0290c8f
Krzysztof Lisocki <90252904+lisocki@users.noreply.github.com>
2023-11-10 10:28:42 +0100

    [AUT-10238] Improved `upsert-templates` target (#11)

--- scripts/diff_from_original.sh
+++ scripts/diff_from_original.sh
@@ -5,29 +5,29 @@ set -e
 # is different from its origin on the original repository.
 # The script exits without error if the local copy is DIFFERENT from the origin.
 
-# Check if an argument is provided
-if [ "$#" -ne 1 ]
+# Check if arguments are provided
+if [ "$#" -ne 2 ]
 then
-    echo "Usage: $0 <file-path>"
+    echo "Usage: $0 <origin-revision> <file-path>"
     exit 1
 fi
 
 # Check if a given file exist
-if [ ! -e "$1" ]
+if [ ! -e "$2" ]
 then
-    echo "Error: '$1' does not exist."
+    echo "Error: file '$2' does not exist."
     exit 1
 fi
 
-GITHUB_BRANCH="dev"
-FILE_PATH="$1"
-RAW_URL="https://raw.githubusercontent.com/cloudentity/acp-theme/$GITHUB_BRANCH/$FILE_PATH"
+GIT_REV="$1"
+FILE_PATH="$2"
+RAW_URL="https://raw.githubusercontent.com/cloudentity/acp-theme/$GIT_REV/$FILE_PATH"
 ORIGINAL_FILE="/tmp/${FILE_PATH//\//_}"
 
 # Download the origin file
-if ! curl --silent --output "$ORIGINAL_FILE" "$RAW_URL"
+if ! curl --silent --fail --output "$ORIGINAL_FILE" "$RAW_URL"
 then
-    echo "Error: Cannot download the original file."
+    echo "Error: Cannot download the original file: $RAW_URL"
     exit 1
 fi
 
@@ -35,7 +35,7 @@ fi
 if cmp --silent "$ORIGINAL_FILE" "$FILE_PATH"; then
     exit 2 # The local file is the same
 else
-    exit 0 # The local file is different - no error
+    exit 0 # The local file is different - standard exit
 fi
 
 # Remove downloaded origin file
